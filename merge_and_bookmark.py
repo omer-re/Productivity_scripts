fba587b2d6fc70fceca00fb5ea44f70e4d3f8f29
Omer Re <39154767+omer-re@users.noreply.github.com>
2022-11-05 19:03:48 +0200

    adding sort by number and dir

--- merge_and_bookmark.py
+++ merge_and_bookmark.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
 from glob import glob
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileMerger, PdfFileReader, utils
 import os
 import fnmatch
 import winsound
 import re
+import operator
+import pandas as pd
+pd.set_option('display.max_rows', None)
 
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
@@ -20,6 +23,35 @@ def find_files(directory, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
 
+def sort_hw_num_then_dir(list_of_paths):
+    list_of_dicts=[]
+    for file_path in list_of_paths:
+        full_path=file_path
+        last_dir=file_path.split("\\")[-2]
+        file_name=os.path.basename(file_path).split(".")[0]
+        
+        hw_number=get_hw_number(file_name)
+        file_dict={"full_path":full_path, "last_dir":last_dir, "file_name":file_name, "hw_number":hw_number}
+        list_of_dicts.append(file_dict)
+        
+    sorted_list_of_dicts=sorted(list_of_dicts, key=operator.itemgetter('hw_number', 'last_dir'))
+    print(pd.DataFrame(sorted_list_of_dicts))
+    sorted_list_of_dicts_paths=[d['full_path'] for d in sorted_list_of_dicts if 'full_path' in d]
+    return sorted_list_of_dicts_paths
+
+def get_hw_number(file_name):
+    
+    p = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
+    if re.search(p, file_name) is not None:
+        for catch in re.finditer(p, file_name):
+            if catch[0]:
+                print (catch[0])
+                if int(catch[0])<30:
+                    if int(catch[0])<10:
+                        return str("0"+catch[0])
+                    return catch[0] # catch is a match object
+            
+
 
 def merge(path, output_filename):
     merger = PdfFileMerger(strict=False)
@@ -27,14 +59,27 @@ def merge(path, output_filename):
     for pdffile in find_files(path, '*.pdf'):
         tree_walk_list.append(pdffile)
 
-    tree_walk_list=natural_sort(tree_walk_list)
-
+    #tree_walk_list=natural_sort(tree_walk_list)
+    
+    tree_walk_list=sort_hw_num_then_dir(tree_walk_list)    
+    
     for pdffile in tree_walk_list:
-        if pdffile == output_filename:
-            continue
-        print(f"Appending: '{pdffile}'")
-        bookmark = os.path.basename(pdffile[:-4])
-        merger.append(pdffile, bookmark)
+        try:
+            
+            if pdffile == output_filename:
+                continue
+            else:
+                print(f"Appending: '{pdffile}'")
+                bookmark = os.path.basename(pdffile[:-4])
+                merger.append(pdffile, bookmark)
+        except (NotImplementedError, utils.PdfReadError):
+            
+            print("##  problem with ",pdffile, " dropping it")
+            #with open(pdffile, mode='rb') as f:        
+            #    reader = PdfFileReader(f)
+            #    if reader.isEncrypted:
+            #        print(pdffile, " is encrypted, skipping")
+            #        continue
     merger.write(output_filename)
     merger.close()
 
